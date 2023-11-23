7d3b719263c2bc76a7d4d4625891bf92feb5d090
Luis Cosio <luisalfonsocosioizcapa@gmail.com>
2023-11-23 12:40:17 -0600

    Added logic to validate image format, size and compression

--- single.py
+++ single.py
@@ -13,9 +13,38 @@ import json
 import psutil
 import torch
 import numpy as np
-from PIL import Image
+from PIL import Image, UnidentifiedImageError
 from resnet50nodown import resnet50nodown
 
+def compress_and_resize_image(image_path, max_size=(1024, 1024)):
+    """
+    Compresses and resizes an image to a manageable size.
+
+    Args:
+        image_path (str): Path to the image file.
+        max_size (tuple): Maximum width and height of the resized image.
+
+    Returns:
+        str: Path to the processed image.
+    """
+    try:
+        # Validate the file format
+        if not image_path.lower().endswith(('.png', '.jpg', '.jpeg', '.webp')):
+            raise ValueError("Unsupported file format. Accepts only JPEG, PNG, and WebP.")
+
+        # Open and process the image
+        with Image.open(image_path) as img:
+            if img.size[0] > max_size[0] or img.size[1] > max_size[1]:
+                # Resize the image only if it's larger than max_size
+                img.thumbnail(max_size, Image.LANCZOS)
+            # Save the processed image in a lossless format
+            processed_image_path = os.path.splitext(image_path)[0] + "_processed.png"
+            img.save(processed_image_path, format='PNG', optimize=True)
+            return processed_image_path
+
+    except UnidentifiedImageError as exc:
+        # Explicitly re-raising with context from the original exception
+        raise ValueError("Invalid image file or path.") from exc
 
 def print_memory_usage():
     """Prints the current memory usage of the process."""
@@ -80,7 +109,8 @@ if __name__ == "__main__":
 
     device = "cuda:0" if is_available_cuda() else "cpu"
 
-    img = Image.open(image_path).convert("RGB")
+    processed_image_path = compress_and_resize_image(image_path)
+    img = Image.open(processed_image_path).convert("RGB")
     img.load()
 
     for model_name in models_config:
